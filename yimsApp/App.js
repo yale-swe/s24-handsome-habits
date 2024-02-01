fecd0b91c98429fe3164eb25018eeccc613cd466
winiboya <w>
2024-02-01 15:20:12 -0500

    background color updated

--- yimsApp/App.js
+++ yimsApp/App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF8F0',
     alignItems: 'center',
     justifyContent: 'center',
   },
