1c86f47da9e5a9fc7b08eec36d295195f230d2ed
winiboya <w>
2024-02-01 15:03:31 -0500

    app setup

---
+++ yimsApp/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is yims. Welcome. Beware.</Text>
+      <Text>Yims is a dangerous place.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
