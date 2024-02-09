88e5570fb8798c3b1966f0e00ac9c24810eb8fc1
J-467 <jotweneboakodua@gmail.com>
2024-02-09 17:12:44 -0500

    Moved user to models; started creating tests

---
+++ server/src/db/db_operations.js
@@ -0,0 +1,4 @@
+import mongoose from "mongoose";
+import User from "./models/user.js";
+
+async function createUser() {}
---
+++ server/src/tests/testUser.js
@@ -0,0 +1,3 @@
+import User from "../db/models/user";
+
+// Create a new user and test that the user is in the db
