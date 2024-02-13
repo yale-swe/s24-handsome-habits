658c3c03e967c8586a102e25b319786c4031e0a4
winiboya <w>
2024-02-13 17:25:04 -0500

    login layout

--- client/App.js
+++ client/App.js
@@ -1,15 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity,  Image } from "react-native";
+import { useEffect, useState } from "react";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [userInfo, setUserInfo] = useState(null);
+  console.log("client Id", process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID);
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
+  });
+
+  useEffect(() => {
+    googleSignin();
+  }, [response]);
+
+  async function googleSignin() {
+    const user = await AsyncStorage.getItem("@user");
+    if (user) {
+      setUserInfo(JSON.parse(user));
+    } else {
+      if (response?.type === "success") {
+        await getUserInfo(response.authentication.accessToken);
+      }
+    }
+  }
+
+  // make na api call to server to get user info
+  async function getUserInfo(token) {
+    if (!token) return;
+
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>This is yims. Welcome. Beware.</Text>
-      <Text>Yims is a dangerous place.</Text>
-      <Image source={require('./src/images/aminata.png')}
-      style = {{ width: 300, height: 400}} />
+      <Text style={styles.header2}>Welcome to</Text>
+      <Text style={styles.header1}>Handsome Habits</Text>
+      <Image
+        source={require("./src/images/bulldog.png")}
+        style={styles.bulldog}
+      />
+      <Text>{JSON.stringify(userInfo, null, 2)}</Text>
+      {console.log("userInfo", userInfo)}
+      <TouchableOpacity onPress={() => promptAsync()} style={styles.loginButton}>
+        <View style={styles.buttonContent}>
+
+          <Image 
+            source={require("./src/images/googlelogo.png")} 
+            style={styles.googleLogo}
+          />
+          <Text style={styles.buttonText}>Sign in with Google</Text>
+        </View>
+      </TouchableOpacity>
+      {/* <Button style={styles.header1} title="Sign in with Google" onPress={() => promptAsync()} /> */}
+      <Button
+        title="Deleted saved users"
+        onPress={() => AsyncStorage.removeItem("@user")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,8 +82,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF8F0',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFF8F0",
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "Roboto"
   },
+  header2: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  header1: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "#FFB706",
+  },
+  bulldog: {
+    margin: 30,
+    width: 150,
+    height: 250,
+  },
+  loginButton: {
+    backgroundColor: "#FFB706",
+    color: "white",
+    fontSize: 20,
+    borderRadius: 10,
+    padding: 0,
+    // border: "3px solid black",
+  },
+  googleLogo:{
+    width: 30,
+    height: 30,
+    backgroundColor: "white",
+    borderBottomStartRadius: 10,
+    borderTopStartRadius: 10,
+  },
+  buttonContent: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    // padding: 0,
+    // margin: 10,
+  },
+  buttonText : {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  }
 });
