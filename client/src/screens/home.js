fe6fca21d5c298c598e5a4b50dd7286b342537bc
winiboya <w>
2024-02-15 09:27:08 -0500

    Revert "navigation to home page with background"
    This reverts commit 868d268a5aa51fb9b60268ae57edfbcdc2b368c2.

--- client/src/screens/home.js
+++ client/src/screens/home.js
@@ -6,7 +6,6 @@ import {
   Button,
   TouchableOpacity,
   Image,
-  ImageBackground
 } from "react-native";
 import { useEffect } from "react";
 
@@ -14,9 +13,7 @@ const Home = (navigation) => {
 
   return (
     <View style={styles.container}>
-        <ImageBackground source={require("../assets/images/cross_bg.png")} resizeMode="cover" style={styles.image}>
-            <Text style={styles.header1}>Home Page</Text>
-        </ImageBackground>
+      <Text style={styles.header1}>Hello</Text>
     </View>
   );
 }
@@ -24,20 +21,9 @@ const Home = (navigation) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFF8F0",
-    // alignItems: "center",
-    // justifyContent: "center",
-    fontFamily: "Roboto",
-  },
-  image: {
-    flex: 1,
-    justifyContent: "center"
-  },
-  header1: {
-    fontSize: 70,
-    fontWeight: "bold",
-    color: "#000",
-    textAlign: "center",
+    backgroundColor: "purple",
+    alignItems: "center",
+    justifyContent: "center",
     fontFamily: "Roboto",
   },
 });
