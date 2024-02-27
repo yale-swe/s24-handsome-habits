be6ada46b6a2bc41035d39c73efdfb0d9436ab6f
Aminata Sakho <aminatasakho@Aminatas-MacBook-Pro.local>
2024-02-27 01:21:30 -0500

    feat: further implementation of db models

---
+++ server/src/db/models/assets.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+/**
+ * Mongoose model for Assets.
+ * 
+ * @requires mongoose
+ * 
+ * @description
+ * Represents a user's Assets in the database 
+ *
+ * 
+ * @typedef {Object} Assets
+ * @property {mongoose.Schema.Types.ObjectId} user_id - user_id for the Assets.
+ * @property {String[]} clothes - list of clothing items a user owns.
+ * @property {String[]} objects - list of objects/trinkets a user owns.
+ * @property {String[]} backgrounds - list of backgrounds owned by the user.
+ */
+
+// Assets for users
+const assetsSchema = new mongoose.Schema({
+    //mongoose creates automatically a unique _id for each row
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        unique: true,
+        ref: 'User'
+    },
+    clothes: [String],
+    objects: [String],
+    backgrounds: [String]
+    
+});
+
+const Assets = mongoose.model('Assets', assetsSchema);
+
+export default Assets;
--- server/src/db/models/pointsTable.js
+++ server/src/db/models/pointsTable.js
@@ -10,7 +10,7 @@ import mongoose from 'mongoose';
  * A table representing a user's coins and wellness points by habit.
  * 
  * @typedef {Object} PointsTable
- * @property {String} user_id - The user's unique id which ties them to the points table.
+ * @property {mongoose.Schema.Types.ObjectId} user_id - The user's unique  object id which ties them to the points table.
  * @property {Int32} coins - The number of coins posessed by the user.
  * @property {Int32} exercise_points - A user's wellness points from logging exercise.
  * @property {Int32} eating_points - A user's wellness points from logging eating.
@@ -18,12 +18,14 @@ import mongoose from 'mongoose';
  * @property {Int32} studying_points - A user's wellness points from logging studying.
  */
 
-// Profile for Yale students
+// reward table schema
 const pointsTableSchema = new mongoose.Schema({
+    //mongoose creates automatically a unique _id for each row
     user_id: {
-        type: String,
+        type: mongoose.Schema.Types.ObjectId,
         required: true,
-        unique: true
+        unique: true,
+        ref: 'User'
     },
     coins: { type: Int32, default: 0 },
     exercise_points: { type: Int32, default: 26},
