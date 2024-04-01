6b18d347c4a0a54d76e3454ac9feabf0bdae46ae
J-467 <jotweneboakodua@gmail.com>
2024-04-01 12:08:11 -0400

    feat: adding function to update points with change

--- client/src/screens/exercise.js
+++ client/src/screens/exercise.js
@@ -3,101 +3,63 @@ import { useFocusEffect } from "@react-navigation/native";
 import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
 import { Typography, Colors, Buttons } from "../styles";
 import PropTypes from "prop-types";
-import { retrieveHabitsByCategory } from "../services/habitService";
-import BackButton from "../components/backButton";
-import WorkoutLogList from "../components/workoutLogList.js"; // Import the WorkoutLogList component
-
-
-
+import QuoteBox from "../components/quoteBox";
 
 // eslint-disable-next-line
 const Exercise = (props) => {
+  Exercise.propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func.isRequired,
+    }).isRequired,
+  };
 
-    Exercise.propTypes = {
-        navigation: PropTypes.shape({
-            navigate: PropTypes.func.isRequired,
-        }).isRequired,
-    };
-
-
-    return ( <
-        View style = { styles.container } >
-        <
-        TouchableOpacity onPress = {
-            () => props.navigation.navigate("Home")
-        }
-        style = { Buttons.habitButton } >
-        <
-        Text > Go Back to Home < /Text> < /
-        TouchableOpacity > <
-        QuoteBox quoteText = "the grind doesn’t stop."
-        image = { require("../assets/images/bulldog_workout.png") } > < /QuoteBox> <
-        View style = { styles.logButtonContainer } >
-
-        <
-        TouchableOpacity onPress = {
-            () => props.navigation.navigate('ExerciseLog')
-        }
-        style = { styles.logButton } >
-        <
-        Text style = { styles.logButtonText } > Log a Workout < /Text> < /
-        TouchableOpacity > <
-        /View>
-
-
-        <
-        /View>
-    );
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate("Home")}
+        style={Buttons.habitButton}
+      >
+        <Text>Go Back to Home</Text>
+      </TouchableOpacity>
+      <QuoteBox
+        quoteText="the grind doesn’t stop."
+        image={require("../assets/images/bulldog_workout.png")}
+      >
+        {" "}
+      </QuoteBox>
+      <View style={styles.logButtonContainer}>
+        <TouchableOpacity
+          onPress={() => props.navigation.navigate("ExerciseLog")}
+          style={styles.logButton}
+        >
+          <Text style={styles.logButtonText}>Log a Workout</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: Colors.Colors.yellow,
-        ...Typography.defaultFont,
-        backgroundColor: Colors.Colors.yellow,
-        ...Typography.defaultFont,
-    },
-    backgroundImage: {
-        flex: 1,
-        justifyContent: "center",
-    },
-    logButtonContainer: {
-        alignItems: "center",
-
-    },
-    logButton: {
-        backgroundColor: Colors.Colors.navy,
-        ...Buttons.logButton,
-    },
-    logButtonText: {
-        color: "white",
-        ...Typography.header4,
-    },
-    logButtonContainer: {
-        alignItems: "center",
-
-    },
-    logButton: {
-        backgroundColor: Colors.Colors.navy,
-        ...Buttons.logButton,
-    },
-    logButtonText: {
-        color: "white",
-        ...Typography.header4,
-    },
-    logButtonContainer: {
-        alignItems: "center",
-
-    },
-    logButton: {
-        backgroundColor: Colors.Colors.navy,
-        ...Buttons.logButton,
-    },
-    logButtonText: {
-        color: "white",
-        ...Typography.header4,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: Colors.Colors.yellow,
+    ...Typography.defaultFont,
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  logButtonContainer: {
+    alignItems: "center",
+  },
+  logButton: {
+    backgroundColor: Colors.Colors.navy,
+    ...Buttons.logButton,
+  },
+  logButtonText: {
+    color: "white",
+    ...Typography.header4,
+  },
 });
 
 export default Exercise;
--- client/src/services/PointsService.js
+++ client/src/services/PointsService.js
@@ -4,20 +4,20 @@ import { StatusCodes } from "http-status-codes";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export async function getPointInfo() {
-    try {
-        const response = await Api.get("/points"); // GET request to find points
-        const rawPoints = response.data.points;
+  try {
+    const response = await Api.get("/points"); // GET request to find points
+    const rawPoints = response.data.points;
 
-        // Adds wellness and emotion values to the points object
-        const userPoints = getQualityPoints(rawPoints);
-        AsyncStorage.setItem("points", JSON.stringify(userPoints));
+    // Adds wellness and emotion values to the points object
+    const userPoints = getQualityPoints(rawPoints);
+    AsyncStorage.setItem("points", JSON.stringify(userPoints));
 
-        return userPoints;
-    } catch (err) {
-        if (err.response.status == StatusCodes.UNAUTHORIZED) {
-            logout(); // Session is expired/invalid, so logout
-        }
+    return userPoints;
+  } catch (err) {
+    if (err.response.status == StatusCodes.UNAUTHORIZED) {
+      logout(); // Session is expired/invalid, so logout
     }
+  }
 }
 
 /**
@@ -33,28 +33,27 @@ export async function getPointInfo() {
  * @returns {JSON} - The points data if updated correctly; otherwise, null.
  */
 export async function updatePoints(newPoints) {
-    try {
-        newPoints.exercise_points = Math.min(newPoints.exercise_points, 26);
-        newPoints.eating_points   = Math.min(newPoints.eating_points, 25);
-        newPoints.sleeping_points = Math.min(newPoints.sleeping_points, 27);
-        newPoints.studying_points = Math.min(newPoints.studying_points, 22);
-        
-        const response = await Api.post("/points/update", {
-            points: newPoints,
-        }); // Post request to update points
+  try {
+    newPoints.exercise_points = Math.min(newPoints.exercise_points, 26);
+    newPoints.eating_points = Math.min(newPoints.eating_points, 25);
+    newPoints.sleeping_points = Math.min(newPoints.sleeping_points, 27);
+    newPoints.studying_points = Math.min(newPoints.studying_points, 22);
 
-        // Save new points in client's local storage
-        AsyncStorage.setItem("points", JSON.stringify(response.data));
+    const response = await Api.post("/points/update", {
+      points: newPoints,
+    }); // Post request to update points
 
-        return response.data;
-    } catch (err) {
-        if (err.response.status == StatusCodes.UNAUTHORIZED) {
-            logout(); // Session is expired/invalid, so logout
-        }
-        return null;
+    // Save new points in client's local storage
+    AsyncStorage.setItem("points", JSON.stringify(response.data));
+
+    return response.data;
+  } catch (err) {
+    if (err.response.status == StatusCodes.UNAUTHORIZED) {
+      logout(); // Session is expired/invalid, so logout
     }
+    return null;
+  }
 }
-
 
 /**
  * @description Update the points of a category by a specified amount.
@@ -62,19 +61,18 @@ export async function updatePoints(newPoints) {
  * @param {Number} pointChange - The change in points.
  * @returns {JSON} - The updated points if successful; otherwise, null.
  */
-export async function updateCategoryPointwithChange(category, pointChange) {
-    let categoryPoints = categoryPointName(category);
-    let currentPoints = JSON.parse(AsyncStorage.getItem("points"));
+export async function updatePointswithChange(category, pointChange) {
+  let categoryPoints = categoryPointName(category);
+  let currentPoints = AsyncStorage.getItem("points");
 
-    if (currentPoints == null) {
-        currentPoints = await getPointInfo();
-    }
+  if (currentPoints == null) {
+    currentPoints = await getPointInfo();
+  }
 
-    currentPoints[categoryPoints] += pointChange;
+  currentPoints[categoryPoints] += pointChange;
 
-    return updatePoints(currentPoints);
+  return updatePoints(currentPoints);
 }
-
 
 /**
  * @param {*} points
@@ -83,37 +81,36 @@ export async function updateCategoryPointwithChange(category, pointChange) {
  * @returns New points object, including the wellness and emotion values
  */
 export const getQualityPoints = (points) => {
-    let wellness =
-        points.exercise_points +
-        points.sleeping_points +
-        points.eating_points +
-        points.studying_points;
+  let wellness =
+    points.exercise_points +
+    points.sleeping_points +
+    points.eating_points +
+    points.studying_points;
 
-    // if wellness is max, slightly decrease it so
-    // that the range of emotion is 0-4
-    wellness = wellness == 100 ? 99 : wellness;
+  // if wellness is max, slightly decrease it so
+  // that the range of emotion is 0-4
+  wellness = wellness == 100 ? 99 : wellness;
 
-    const emotion = Math.floor(wellness / 20);
+  const emotion = Math.floor(wellness / 20);
 
-    points.wellness_points = wellness;
-    points.emotion_value = emotion;
-    return points;
-}
-
+  points.wellness_points = wellness;
+  points.emotion_value = emotion;
+  return points;
+};
 
 // toLower the category name and concatenate with "_points"
 /** Returns the name of the category point in the database */
 const categoryPointName = (category) => {
-    switch (category) {
-        case "exercise":
-            return "exercise_points";
-        case "eating":
-            return "eating_points";
-        case "sleeping":
-            return "sleeping_points";
-        case "studying":
-            return "studying_points";
-        default:
-            return null;
-    }
+  switch (category) {
+    case "exercise":
+      return "exercise_points";
+    case "eating":
+      return "eating_points";
+    case "sleeping":
+      return "sleeping_points";
+    case "studying":
+      return "studying_points";
+    default:
+      return null;
+  }
 };
