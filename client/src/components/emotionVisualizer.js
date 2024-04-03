35a700da8d8e48df3875b1cc77657bdd6066877e
J-467 <jotweneboakodua@gmail.com>
2024-04-03 18:50:37 -0400

    feat: adding new files

---
+++ client/src/components/emotionVisualizer.js
@@ -0,0 +1,36 @@
+import { StyleSheet, View } from "react-native";
+import { Image } from "react-native";
+// import PropTypes from "prop-types"; can add back once we actually do variable emotion
+
+const EmotionVisualizer = () => {
+  return (
+    <View style={styles.bulldogContainer}>
+      <Image
+        source={require("../assets/images/bulldog.png")}
+        style={styles.bulldog}
+      />
+    </View>
+  );
+};
+
+// WellnessBar.propTypes = {
+//     emotion: PropTypes.any,
+//   }; for julian :)
+
+const styles = StyleSheet.create({
+  bulldog: {
+    margin: 30,
+    width: 200,
+    height: 300,
+  },
+  bulldogContainer: {
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    padding: 10,
+    borderRadius: 20,
+    width: 258,
+    height: 394,
+    bottom: 20,
+  },
+});
+
+export default EmotionVisualizer;
--- client/src/styles/typography.js
+++ client/src/styles/typography.js
@@ -1,43 +1,56 @@
-export const defaultFont = { fontFamily: "OpenSans-Regular"};
+export const defaultFont = { fontFamily: "OpenSans-Regular" };
 export const boldFont = { fontFamily: "OpenSans-Bold" };
 export const semiBoldFont = { fontFamily: "OpenSans-SemiBold" };
 export const passion = { fontFamily: "PassionOne-Regular" };
 export const passionBold = { fontFamily: "PassionOne-Bold" };
-export const boldItalic = {fontFamily: "OpenSans-BoldItalic", fontSize:28};
+export const boldItalic = { fontFamily: "OpenSans-BoldItalic", fontSize: 28 };
 
 export const header1 = {
-    fontSize: 70,
-    color: "#000",
-    textAlign: "center",
-    ...boldFont
+  fontSize: 70,
+  color: "#000",
+  textAlign: "center",
+  ...boldFont,
 };
 
 export const header3 = {
-    fontSize: 36,
-    textAlign: "center",
-    ...boldFont,
+  fontSize: 36,
+  textAlign: "center",
+  ...boldFont,
 };
 
 export const header4 = {
-    fontSize: 20,
-    textAlign: "center",
-    ...boldFont,
+  fontSize: 20,
+  textAlign: "center",
+  ...boldFont,
 };
 
 export const header5 = {
-    fontSize: 16,
-    textAlign: "center",
-    ...boldFont,
+  fontSize: 16,
+  textAlign: "center",
+  ...boldFont,
 };
 
 export const header6 = {
-    fontSize: 14,
-    textAlign: "center",
-    ...boldFont,
+  fontSize: 14,
+  textAlign: "center",
+  ...boldFont,
 };
 
 export const buttonText = {
-    color: "white",
-    fontSize: 20,
-    ...boldFont,
+  color: "white",
+  fontSize: 20,
+  ...boldFont,
+};
+
+export const message = {
+  fontSize: 15,
+  textAlign: "center",
+  // fontStyle: "italic", it looks like this might not have an italic font
+  ...boldFont,
+};
+
+export const coins = {
+  fontSize: 15,
+  textAlign: "center",
+  color: "#FFB706",
 };
