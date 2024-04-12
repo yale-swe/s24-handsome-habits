890fe666309adcb92c6acb2ba9e148a935a6913b
J-467 <jotweneboakodua@gmail.com>
2024-04-12 15:09:19 -0400

    feat: adding and retrieving assets

--- server/src/routes/ApiRouter.js
+++ server/src/routes/ApiRouter.js
@@ -1,13 +1,18 @@
 import { Router } from "express";
 import { findUser, UserFromRequest } from "../controllers/userController.js";
 import { updatePoints, findPoints } from "../controllers/pointsController.js";
-import { addHabit, retrieveHabitsByCategory, deleteHabit} from "../controllers/habitController.js";
+import {
+  addHabit,
+  retrieveHabitsByCategory,
+  deleteHabit,
+} from "../controllers/habitController.js";
 import { StatusCodes } from "http-status-codes";
 
 const router = Router();
 
 router.get("/user", findUser);
 
+/* ************ Points Routes ************ */
 // Update a user's points
 router.post("/points/update", async (req, res) => {
   const user = await UserFromRequest(req); // Get user from request
@@ -28,6 +33,28 @@ router.post("/points/update", async (req, res) => {
   return res.status(StatusCodes.OK).json({ points: updatedPoints });
 });
 
+// Get a User's points
+router.get("/points", async (req, res) => {
+  const user = await UserFromRequest(req);
+  if (!user) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ error: "User not authenticated" });
+  }
+
+  const foundPoints = await findPoints(user._id);
+  if (!foundPoints) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Error finding points" });
+  }
+  console.log("Found points: ", foundPoints);
+  return res.status(StatusCodes.OK).json({ points: foundPoints });
+});
+
+/* ************ Habits Routes ************ */
+
+// Route to add habits by category for a user
 router.post("/habits/add", async (req, res) => {
   const user = await UserFromRequest(req); // Get user from request
   if (!user) {
@@ -47,7 +74,8 @@ router.post("/habits/add", async (req, res) => {
   return res.status(StatusCodes.OK).json({ habit: newHabit });
 });
 
-router.get("/points", async (req, res) => {
+// Route to get habits by category for a user
+router.get("/habits/:categoryName", async (req, res) => {
   const user = await UserFromRequest(req);
   if (!user) {
     return res
@@ -55,34 +83,20 @@ router.get("/points", async (req, res) => {
       .json({ error: "User not authenticated" });
   }
 
-  const foundPoints = await findPoints(user._id);
-  if (!foundPoints) {
-    return res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ error: "Error finding points" });
-  }
-  console.log("Found points: ", foundPoints);
-  return res.status(StatusCodes.OK).json({ points: foundPoints });
-});
-
-
-// Route to get habits by category for a user
-router.get("/habits/:categoryName", async (req, res) => {
-  const user = await UserFromRequest(req);
-  if (!user) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({ error: "User not authenticated" });
-  }
-
   const categoryName = req.params.categoryName;
   try {
     const habits = await retrieveHabitsByCategory(user._id, categoryName);
     if (habits.length === 0) {
-      return res.status(StatusCodes.NOT_FOUND).json({ message: "No habits found for this category" });
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "No habits found for this category" });
     }
     return res.status(StatusCodes.OK).json({ habits });
   } catch (err) {
     console.error(err);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Error retrieving habits" });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Error retrieving habits" });
   }
 });
 
@@ -90,20 +104,34 @@ router.get("/habits/:categoryName", async (req, res) => {
 router.delete("/habits/:habitId", async (req, res) => {
   const user = await UserFromRequest(req);
   if (!user) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({ error: "User not authenticated" });
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ error: "User not authenticated" });
   }
 
   const habitId = req.params.habitId;
   try {
     const success = await deleteHabit(habitId);
     if (!success) {
-      return res.status(StatusCodes.NOT_FOUND).json({ error: "Habit not found or error deleting habit" });
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: "Habit not found or error deleting habit" });
     }
-    return res.status(StatusCodes.OK).json({ message: "Habit deleted successfully" });
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: "Habit deleted successfully" });
   } catch (err) {
     console.error(err);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Error deleting habit" });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Error deleting habit" });
   }
 });
+
+/* ************ Assets Routes ************ */
+
+router.get("/assets", async (req, res) => {});
+
+router.post("/assets/update", async (req, res) => {});
 
 export default router;
