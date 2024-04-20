2c548ffb6f3072bb6c1bd89fe6c84065b401d9ad
Aminata Sakho <aminatasakho@Aminatas-MacBook-Pro.local>
2024-04-20 18:32:08 -0400

    feat: dan barks in home screen when tapped

--- client/src/components/HandsomeDan.js
+++ client/src/components/HandsomeDan.js
@@ -1,7 +1,7 @@
 import { getClothesPath, getEmotionPath } from "../services/AppearanceService";
 import { useFocusEffect } from "@react-navigation/native";
-import { StyleSheet, Image, View } from "react-native";
-import { Colors } from "../styles";
+import { StyleSheet, Image, View, TouchableWithoutFeedback, Text } from "react-native";
+import { Colors, Typography } from "../styles";
 import { body } from "../constants/resources";
 import React, { useEffect, useState } from "react";
 
@@ -9,6 +9,7 @@ const HandsomeDan = () => {
 
     const [clothes, setClothes] = useState({});
     const [emotion, setEmotion] = useState(null);
+    const [showBubble, setShowBubble] = useState(false);
 
     async function fetchData() {
         const clothesPath = await getClothesPath();
@@ -29,15 +30,27 @@ const HandsomeDan = () => {
         }, [])
       );
 
+    const handlePress = () => {
+        setShowBubble(true);
+        setTimeout(() => setShowBubble(false), 2000); // Bubble disappears after 2 seconds
+    };
+
     return (
         <View style={styles.container}>
+            <TouchableWithoutFeedback style={styles.imageContainer} onPress={handlePress}>
             <View style={styles.imageContainer}>
                 <Image source={body} style={styles.image} />
                 <Image source={emotion} style={styles.image} />
                 <Image source={clothes.top} style={styles.image} />
                 <Image source={clothes.bottom} style={styles.image} />
                 {clothes.accessories && <Image source={clothes.accessories} style={styles.image} />}
+                {showBubble && (
+                    <View style={styles.bubble}>
+                        <Text style={Typography.defaultFont}>Woof!</Text>
+                    </View>
+                )}
             </View>
+            </TouchableWithoutFeedback>
         </View>
     );
 };
@@ -67,6 +80,20 @@ const styles = StyleSheet.create({
         top: 0,
         left: "-10%", // center horizontally
     },
+    bubble: {
+        position: "absolute",
+        top: 10, // Lower and further to the left
+        left: "70%", // Moving it to the left from the center
+        transform: [{ translateX: -50 }],
+        backgroundColor: "white",
+        borderRadius: 30,
+        padding: 8,
+        width: 100, // Increased width
+        height: 50, // Increased height
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 1
+    }
 
 });
 
